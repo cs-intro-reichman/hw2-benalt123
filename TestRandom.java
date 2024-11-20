1cc502994e27836491b452ec140aba0e693d0291
benalt123 <benben96363@gmail.com>
2024-11-20 15:48:34 +0200

    completed

--- TestRandom.java
+++ TestRandom.java
@@ -2,5 +2,28 @@
 public  class  TestRandom {
 	public static void main(String[]  args) {
 	    // Replace this comment with your code
+		int times = Integer.parseInt(args[0]);
+		int less = 0;
+		int more = 0;
+		for(int i = 0; i <times; i++)
+		{
+			double num = Math.random();
+			if(num>0.5)
+				more++;
+			else
+				less++;
+		}
+		System.out.println("> 0.5: " + more + " times ");
+		System.out.println("<= 0.5: " + less + " times ");
+		if(more>0 && less>0);
+		{
+			double max = (double) Math.max(more,less);
+			double min = (double) Math.min(more,less);
+			double ratio = min/max;
+			System.out.println("Ratio: " + ratio);
+		}
+		
+
+
 	}
 }
